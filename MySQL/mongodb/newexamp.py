c0c00134b8b6313e864fca24200478b35fabc41a
Shrishty02 <shrishty02singh@gmail.com>
2022-07-17 22:31:31 +0530

    THis is my github push test

--- MySQL/mongodb/newexamp.py
+++ MySQL/mongodb/newexamp.py
@@ -3,13 +3,12 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://Shrishty:Shrishty123@cluster0.i8wwq.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
+print('hello cutieeee')
 
 d ={
     "name": "shrishty",
     "email" : "shrishty02singh@gmail.com",
     "surname": "singh"
-
-
 }
 db1 = client['mongoddtest']
 coll = db1['test']
